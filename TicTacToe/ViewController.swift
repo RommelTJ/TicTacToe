7d6bd4133ce421ac4f7b3d052ddae3d64df75a87
Rommel Rico <rommeltj@gmail.com>
2015-03-02 11:11:05 -0800

    Initial layout

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func doButton0(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton1(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton2(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton3(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton4(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton5(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton6(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton7(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func doButton8(sender: AnyObject) {
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
